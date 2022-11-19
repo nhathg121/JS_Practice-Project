e714e406e105d96033e8dff854078b6ca0f73e2c
Nhathg99 <nhathg99@gmai.com>
2022-11-19 11:35:46 +0700

    add Not-interested button

--- 15_Project_react/2-tours/src/App.js
+++ 15_Project_react/2-tours/src/App.js
@@ -7,7 +7,10 @@ const url = "https://course-api.com/react-tours-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
-
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id != id);
+    setTours(newTours);
+  };
   //======================= Fetch API =================
   const fetchTours = async () => {
     setLoading(true);
@@ -35,7 +38,7 @@ function App() {
   }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
--- 15_Project_react/2-tours/src/Tour.js
+++ 15_Project_react/2-tours/src/Tour.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Tour = ({ id, image, info, price, name }) => {
+const Tour = ({ id, image, info, price, name, removeTour }) => {
   const [readMore, setreadMore] = useState(false);
   return (
     <article className="single-tour">
@@ -17,7 +17,9 @@ const Tour = ({ id, image, info, price, name }) => {
           </button>
         </p>
 
-        <button className="delete-btn">not interested</button>
+        <button onClick={() => removeTour(id)} className="delete-btn">
+          not interested
+        </button>
       </footer>
     </article>
   );
